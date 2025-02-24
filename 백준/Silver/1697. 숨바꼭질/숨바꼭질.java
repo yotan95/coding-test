3265942c0d4e0f96f9ac1d6f8fe6f76987e99a60
yotan95 <69889349+yotan95@users.noreply.github.com>
2025-02-24 14:52:17 +0900

    [Silver I] Title: 숨바꼭질, Time: 180 ms, Memory: 17816 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -2,69 +2,69 @@ import java.util.*;
 import java.io.*;
 
 public class Main{
-    static Integer N, target;
-    static int time[];
     
-    private static class Node implements Comparable<Node>{
-        Integer position;
-        Integer time;
-        
-        Node(Integer position, Integer time){
+   static int n, k;
+    static int[] dir;
+
+    public static class Node implements Comparable<Node>{
+        int position;
+        int dir;
+
+        Node(int position, int dir){
             this.position = position;
-            this.time = time;
+            this.dir = dir;
+        }
+
+        @Override
+        public int compareTo(Node d){
+            return this.dir - d.dir;
         }
         
-        @Override
-        public int compareTo(Node e){
-            return this.time - e.time;
-        }
     }
-    
+
     public static void main(String[] args) throws IOException{
         readData();
-        System.out.print(pro(N, target));
+        pro();
+        System.out.print(dir[k]);
     }
-    
-    private static Integer pro(Integer start, Integer target){
-        PriorityQueue<Node> queue = new PriorityQueue<Node>();
-        queue.add(new Node(start, 0));
-        time[start] = 0;
-        
-        while(!queue.isEmpty()){
-            Node current = queue.poll();
-            Integer currentPosition = current.position;
-            Integer currentTime = current.time;
-            
-            if(time[currentPosition] < currentTime) continue;
-            
-            if(currentPosition -1 >= 0 && time[currentPosition-1] > currentTime +1){
-                time[currentPosition-1] = currentTime+1;
-                queue.add(new Node(currentPosition -1, currentTime +1));
-            }
-            
-            if(currentPosition +1 <= 100000 && time[currentPosition+1] > currentTime+1){
-                time[currentPosition+1] = currentTime+1;
-                queue.add(new Node(currentPosition +1, currentTime +1));
-            }
-            
-            if(currentPosition *2 <= 100000 && time[currentPosition*2] > currentTime+1){
-                time[currentPosition * 2] = currentTime +1;
-                queue.add(new Node(currentPosition * 2, currentTime+1));
-            }
-        }
-        
-        return time[target];
-    }
-    
-    private static void readData() throws IOException{
+
+    public static void readData() throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] s = br.readLine().split(" ");
-        N = Integer.parseInt(s[0]);
-        target = Integer.parseInt(s[1]);
-        time = new int[100001];
-        
-        Arrays.fill(time, Integer.MAX_VALUE);
+        dir = new int[100001];
+        n = Integer.parseInt(s[0]);
+        k = Integer.parseInt(s[1]);
+
+        Arrays.fill(dir, Integer.MAX_VALUE);
     }
-    
+
+    public static void pro(){
+        PriorityQueue<Node> q = new PriorityQueue<Node>();
+        q.add(new Node(n, 0));
+        dir[n] = 0;
+
+        while(!q.isEmpty()){
+            Node node = q.poll();
+            int currentPosition = node.position;
+            int currentDir = node.dir;
+
+            if(isValid(currentPosition - 1) && dir[currentPosition-1] > currentDir + 1){
+                dir[currentPosition-1] = currentDir + 1;
+                q.add(new Node(currentPosition - 1, currentDir+1));
+            }
+            if(isValid(currentPosition +1) && dir[currentPosition +1] > currentDir+1){
+                dir[currentPosition+1] = currentDir+1;
+                q.add(new Node(currentPosition+1, currentDir+1));
+            }
+            if(isValid(currentPosition*2) && dir[currentPosition*2] > currentDir+1){
+                dir[currentPosition*2] = currentDir+1;
+                q.add(new Node(currentPosition*2, currentDir+1));
+            }
+        }
+    }
+
+    public static boolean isValid(int x){
+        return x >= 0 && x < 100001;
+    }
     
 }

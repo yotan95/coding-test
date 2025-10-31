f35f502afc5e5db3f84cf9082d6012f3ec9769e6
yotan95 <69889349+yotan95@users.noreply.github.com>
2025-10-31 21:03:06 +0900

    [Silver I] Title: 경로 찾기, Time: 156 ms, Memory: 18912 KB -BaekjoonHub

--- 백준/Silver/11403. 경로 찾기/경로 찾기.java
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.java
@@ -2,70 +2,74 @@ import java.util.*;
 import java.io.*;
 
 public class Main{
+    
     static int n;
+    static HashMap<Integer, ArrayList<Integer>> graph;
     static boolean[][] visit;
-    static HashMap<Integer, ArrayList<Integer>> map = new HashMap<Integer, ArrayList<Integer>>();
-    static StringBuilder sb = new StringBuilder();
-        
+    static StringBuilder sb;
+    
     public static void main(String[] args) throws IOException{
         readData();
         pro();
-        System.out.print(sb.toString());
     }
     
-    private static void readData() throws IOException{
+    public static void readData() throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
         visit = new boolean[n][n];
+        graph = new HashMap<Integer, ArrayList<Integer>>();
+        sb = new StringBuilder();
         
-        for(int i =0 ; i < n; i++){
+        for(int i = 0 ; i < n ; i++){
             String[] s = br.readLine().split(" ");
-            for(int j = 0 ; j < n; j++){
+            for(int j = 0 ; j < s.length; j++){
                 int node = Integer.parseInt(s[j]);
                 if(node == 1){
-                    map.putIfAbsent(i, new ArrayList<Integer>());
-                    
-                    map.get(i).add(j);
+                    graph.putIfAbsent(i, new ArrayList<Integer>());
+                    graph.get(i).add(j);
                 }
             }
         }
     }
     
-    private static void pro(){
-        for(int key : map.keySet()){
-            ArrayList<Integer> list = map.get(key);
-            if(list != null){
-                for(int i = 0 ; i < list.size(); i++){
-                    int node = list.get(i);
-                    if(!visit[key][node]){
-                        dfs(key, node);
-                    }
+    public static void pro(){
+         for(int i = 0; i < n; i++){
+             for(int j = 0; j < n; j++){
+                visit[i][j] = false;
+             }
+             if(graph.containsKey(i)){
+                 ArrayList<Integer> list = graph.get(i);
+                 for(int node : list){
+                     if(!visit[i][node]){
+                         dfs(i, node);
+                     }
                 }
             }
         }
+
         
-        for(int i = 0 ; i < visit.length; i++){
+        for(int i = 0; i < visit.length; i++){
             for(int j = 0 ; j < visit[i].length; j++){
                 if(visit[i][j]){
                     sb.append(1).append(" ");
-                }else{
+                } else{
                     sb.append(0).append(" ");
                 }
             }
             sb.append("\n");
         }
         
+        System.out.print(sb.toString());
     }
     
-    private static void dfs(int key, int node){
-        visit[key][node] = true;
-        
-        ArrayList<Integer> list = map.get(node);
-        if(list != null){
-            for(int i = 0 ; i < list.size(); i++){
-                int el = list.get(i);
-                if(!visit[key][el]){
-                    dfs(key, el);
+    public static void dfs(int start, int node){
+        visit[start][node] = true;
+    
+        if(graph.containsKey(node)){
+            ArrayList<Integer> list = graph.get(node);
+            for(int next : list){
+                if(!visit[start][next]){
+                    dfs(start, next);
                 }
             }
         }

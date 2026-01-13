d2a2d69ae652de795f601e8c20cb919314f4e156
yotan95 <69889349+yotan95@users.noreply.github.com>
2026-01-13 21:52:01 +0900

    [level 2] Title: 피보나치 수, Time: 4.77 ms, Memory: 89.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int n) {
+        int m = 1234567;
+        
+        int[] dp = new int[n+1];
+        dp[0] = 0;
+        dp[1] = 1;
+        
+        for(int i = 2; i<= n; i++){
+            dp[i] = (dp[i-2] + dp[i-1]) %m;
+        }
+        return dp[n];
+    }
+}
